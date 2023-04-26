778d74ab40c0c91ea239c773ebd8b5d747a7e07f
Alissonv2 <alissonvitorino8@gmail.com>
2023-04-26 11:36:30 -0300

    refactor: change page components styles

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,6 +4,7 @@ import { GetStaticProps } from "next"
 import Stripe from "stripe"
 import { useKeenSlider } from 'keen-slider/react'
 import { HomeContainer, Product } from "../styles/pages/home"
+import Link from "next/link"
 
 import 'keen-slider/keen-slider.min.css'
 
@@ -12,7 +13,7 @@ interface HomeProps {
     id: string;
     name: string;
     imageUrl: string;
-    price: number;
+    price: string;
   }[]
 }
 
@@ -28,16 +29,23 @@ export default function Home({ products }: HomeProps) {
       {
         products?.map((product) => {
           return (
-            <Product key={product.id} className="keen-slider__slide">
-              <Image src={product.imageUrl} width={520} height={480} alt="" />
+            <Link href={`/product/${product.id}`} key={product.id}>
+              <Product className="keen-slider__slide">
+                <Image
+                  src={product.imageUrl}
+                  width={520}
+                  height={480}
+                  alt=""
+                />
 
-              <footer>
-                <strong>
-                  {product.name}
-                </strong>
-                <span>{product.price}</span>
-              </footer>
-            </Product>
+                <footer>
+                  <strong>
+                    {product.name}
+                  </strong>
+                  <span>{product.price}</span>
+                </footer>
+              </Product>
+            </Link>
           )
         })
       }
